c7ae12721a685049ab1f41dcfbe2b37228331adc
Shashwat Kumar <shashwatkmr.001@gmail.com>
2019-06-14 00:31:13 +0530

    data.json parameters are being read and new qr code is not generated

--- client.py
+++ client.py
@@ -3,16 +3,18 @@
 # WS client example
 
 import websocket
-import os;
-import base64;
-import datetime;
-import curve25519;
-import json;
-import pyqrcode;
-import io;
+import os
+import base64
+import datetime
+import curve25519
+import json
+import pyqrcode
+import io
+import random
 
-from utilities import *;
-from threading import Timer;
+from utilities import *
+from threading import Timer
+from os.path import expanduser
 
 try:
     import thread
@@ -20,80 +22,147 @@ except ImportError:
     import _thread as thread
 import time
 
-clientId = base64.b64encode(os.urandom(16));
-privateKey = curve25519.Private();
-publicKey = privateKey.get_public();
-settingsDir = "~/.wweb"
+home = expanduser("~")
+settingsDir = home + "/.wweb"
+settingsFile = settingsDir + '/data.json'
 
 if not os.path.exists(settingsDir):
     os.makedirs(settingsDir)
 
-def sendKeepAlive(ws):
-    Timer(25, lambda: ws.send('?,,')).start()
+class WhatsApp:
 
-def saveSession(jsonObj):
-    print(jsonObj)
-    settingsFile = settingsDir + '/data.json';
-    print("Settings file: ", settingsFile)
-    with open(settingsFile, 'w') as outfile:
-        json.dump(jsonObj, outfile)
+    ws = None
+    mydata = None
+    clientId = None
+    privateKey = None
+    publicKey = None
+    secret = None
+    encKey = None
+    macKey = None
+    sharedSecret = None
+    data = {}
+    mydata = {}
+    sessionExists = False
 
-def on_message(ws, message):
-    try:
-        print("Raw msg: ", message)
-        messageSplit = message.split(",", 1);
-        messageTag = messageSplit[0];
-        messageContent = messageSplit[1];
-        print("Message Tag", messageTag)
-
-        try:
-            jsonObj = json.loads(messageContent);
-        except:
-            print("Error in loading messagecontent")
+    def initLocalParams(self):
+        print('Entering Initlocalparms')
+        self.data = self.restoreSession()
+        keySecret = None
+        if self.data is None:
+            self.mydata['clientId'] = base64.b64encode(os.urandom(16))
+            keySecret = os.urandom(32)
+            self.mydata["keySecret"] = base64.b64encode(keySecret)
+            
         else:
-            if 'ref' in jsonObj:
-                serverRef = json.loads(messageContent)["ref"];
-                qrCodeContents = serverRef + "," + base64.b64encode(publicKey.serialize()) + "," + clientId;
-                svgBuffer = io.BytesIO();											# from https://github.com/mnooner256/pyqrcode/issues/39#issuecomment-207621532
-                img = pyqrcode.create(qrCodeContents, error='L');
-                img.svg(svgBuffer, scale=6, background="rgba(0,0,0,0.0)", module_color="#122E31", quiet_zone=0);
-                print(img.terminal(quiet_zone=1))
-            elif isinstance(jsonObj, list) and len(jsonObj) > 0:
-                if jsonObj[0] == "Conn":
-                    print("Connection msg received")
-                    sendKeepAlive(ws)
-                    saveSession(jsonObj[1])
-                    
+            self.sessionExists = True
+            self.mydata = self.data['myData']
+            keySecret = base64.b64decode(self.mydata["keySecret"])
 
+        print('Keysecret', keySecret)
+        self.clientId = self.mydata['clientId']
+        self.privateKey = curve25519.Private(secret=keySecret)
+        self.publicKey = self.privateKey.get_public()
+        print('Privatekey',self.privateKey)
+        print('ClientId', self.clientId)
+        print('Exiting Initlocalparms')
 
+    def sendKeepAlive(self):
+        Timer(25, lambda: self.ws.send('?,,')).start()
 
+    def saveSession(self, jsonObj):
+        jsonObj['myData'] = self.mydata
+        print(jsonObj)
+        print("Settings file: ", settingsFile)
+        with open(settingsFile, 'w') as outfile:
+            json.dump(jsonObj, outfile)
 
-    except:
-        print("Some error encountered")
-        raise
+    def restoreSession(self):
+        if(os.path.exists(settingsFile)):
+            with open(settingsFile) as file:
+                data = json.load(file)
+                return data
+        return None
 
-def on_error(ws, error):
-    print(error)
+    def setConnInfoParams(self, secret):
+        self.secret = secret
+        self.sharedSecret = self.privateKey.get_shared_key(curve25519.Public(secret[:32]), lambda a: a)
+        sharedSecretExpanded = HKDF(self.sharedSecret, 80)
+        hmacValidation = HmacSha256(sharedSecretExpanded[32:64], secret[:32] + secret[64:])
+        if hmacValidation != secret[32:64]:
+            raise ValueError("Hmac mismatch")
 
-def on_close(ws):
-    print("### closed ###")
+        keysEncrypted = sharedSecretExpanded[64:] + secret[64:]
+        keysDecrypted = AESDecrypt(sharedSecretExpanded[:32], keysEncrypted)
+        self.encKey = keysDecrypted[:32]
+        self.macKey = keysDecrypted[32:64]
 
-def on_open(ws):
+    def on_message(self, ws, message):
+        try:
+            messageSplit = message.split(",", 1)
+            messageTag = messageSplit[0]
+            messageContent = messageSplit[1]
+            print("Message Tag", messageTag)
+
+            try:
+                jsonObj = json.loads(messageContent)
+                print("Raw msg: ", message)
+            except:
+                print("Error in loading message and messagecontent")
+            else:
+                if 'ref' in jsonObj:
+                    if self.sessionExists is False:
+                        serverRef = json.loads(messageContent)["ref"]
+                        qrCodeContents = serverRef + "," + base64.b64encode(self.publicKey.serialize()) + "," + self.clientId
+                        svgBuffer = io.BytesIO();											# from https://github.com/mnooner256/pyqrcode/issues/39#issuecomment-207621532
+                        img = pyqrcode.create(qrCodeContents, error='L')
+                        img.svg(svgBuffer, scale=6, background="rgba(0,0,0,0.0)", module_color="#122E31", quiet_zone=0)
+                        print(img.terminal(quiet_zone=1))
+                elif isinstance(jsonObj, list) and len(jsonObj) > 0:
+                    if jsonObj[0] == "Conn":
+                        print("Connection msg received")
+                        self.sendKeepAlive()
+                        if self.sessionExists is False:
+                            self.setConnInfoParams(base64.b64decode(jsonObj[1]["secret"]))
+                        self.saveSession(jsonObj[1])
+
+        except:
+            print("Some error encountered")
+            raise
+
+    def on_error(self, ws, error):
+        print(error)
+
+    def on_close(self, ws):
+        print("### closed ###")
+
+    def on_open(self, ws):
+        print("Socket Opened")
+        print("ClientId", self.clientId)
+        messageTag = str(getTimestamp())
+        if self.data is not None:
+            clientToken = self.data["clientToken"]
+            serverToken = self.data["serverToken"]
+            message = ('%s,["admin","login","%s","%s","%s","takeover"]' % (messageTag, clientToken, serverToken, self.clientId))
+            print(message)
+            ws.send(message)
+        else:
+            print("No data")
+        message = messageTag + ',["admin","init",[0,3,2390],["Chromium at ' + datetime.datetime.now().isoformat() + '","Chromium"],"' + self.clientId + '",true]'
+        print(message)
+        ws.send(message)
     
-    messageTag = str(getTimestamp());
-    message = messageTag + ',["admin","init",[0,3,2390],["Chromium at ' + datetime.datetime.now().isoformat() + '","Chromium"],"' + clientId + '",true]';
-    print(message)
-    ws.send(message)
+    def connect(self):
+        self.initLocalParams()
+        websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp("wss://w1.web.whatsapp.com/ws/",
+                                on_message = lambda ws,msg: self.on_message(ws, msg),
+                                on_error = lambda ws, msg: self.on_error(ws, msg),
+                                on_close = lambda ws: self.on_close(ws),
+                                on_open = lambda ws: self.on_open(ws),
+                                header = { "Origin: https://web.whatsapp.com" })
+
+        self.ws.run_forever()
 
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://w1.web.whatsapp.com/ws/",
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close,
-                              header = { "Origin: https://web.whatsapp.com" })
-
-    
-    ws.on_open = on_open
-    ws.run_forever()
+    WhatsApp().connect()
--- utilities.py
+++ utilities.py
@@ -1,54 +1,101 @@
-from __future__ import print_function;
+from __future__ import print_function
 
-import sys;
-import time;
+import sys
+import time
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+import hashlib
+import hmac
+import os
 
 
 
 def eprint(*args, **kwargs):							# from https://stackoverflow.com/a/14981125
-	print(*args, file=sys.stderr, **kwargs);
+	print(*args, file=sys.stderr, **kwargs)
 
 def getTimestamp():
-	return int(time.time());
+	return int(time.time())
 
 def getTimestampMs():
-	return int(round(time.time() * 1000));
+	return int(round(time.time() * 1000))
 
 
 
 def mergeDicts(x, y):									# from https://stackoverflow.com/a/26853961
 	if x is None and y is None:
-		return;
-	z = (y if x is None else x).copy();
+		return
+	z = (y if x is None else x).copy()
 	if x is not None and y is not None:
-		z.update(y);
-	return z;
+		z.update(y)
+	return z
 
 def getAttr(obj, key, alt=None):
-	return obj[key] if isinstance(obj, dict) and key in obj else alt;
+	return obj[key] if isinstance(obj, dict) and key in obj else alt
 
 def filterNone(obj):
 	if isinstance(obj, dict):
-		return dict((k, filterNone(v)) for k, v in obj.iteritems() if v is not None);
+		return dict((k, filterNone(v)) for k, v in obj.iteritems() if v is not None)
 	elif isinstance(obj, list):
-		return [filterNone(entry) for entry in obj];
+		return [filterNone(entry) for entry in obj]
 	else:
-		return obj;
+		return obj
 
 def getNumValidKeys(obj):
-	return len(filter(lambda x: obj[x] is not None, list(obj.keys())));
+	return len(filter(lambda x: obj[x] is not None, list(obj.keys())))
 
 def encodeUTF8(s):
 	if not isinstance(s, str):
-		s = strng.encode("utf-8");
-	return s;
+		s = strng.encode("utf-8")
+	return s
 
 
 
 def ceil(n):											# from https://stackoverflow.com/a/32559239
-	res = int(n);
-	return res if res == n or n < 0 else res+1;
+	res = int(n)
+	return res if res == n or n < 0 else res+1
 
 def floor(n):
-	res = int(n);
-	return res if res == 0 or n >= 0 else res-1;
+	res = int(n)
+	return res if res == 0 or n >= 0 else res-1
+
+def HmacSha256(key, sign):
+    return hmac.new(key, sign, hashlib.sha256).digest()
+
+def HKDF(key, length, appInfo=""):						# implements RFC 5869, some parts from https://github.com/MirkoDziadzka/pyhkdf
+    key = HmacSha256("\0"*32, key)
+    keyStream = ""
+    keyBlock = ""
+    blockIndex = 1
+    while len(keyStream) < length:
+        keyBlock = hmac.new(key, msg=keyBlock+appInfo+chr(blockIndex), digestmod=hashlib.sha256).digest()
+        blockIndex += 1
+        keyStream += keyBlock
+    return keyStream[:length]
+
+def AESPad(s):
+    bs = AES.block_size
+    return s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+
+def to_bytes(n, length, endianess='big'):
+    h = '%x' % n
+    s = ('0'*(len(h) % 2) + h).zfill(length*2).decode('hex')
+    return s if endianess == 'big' else s[::-1]
+
+def AESUnpad(s):
+    return s[:-ord(s[len(s)-1:])]
+
+def AESEncrypt(key, plaintext):							# like "AESPad"/"AESUnpad" from https://stackoverflow.com/a/21928790
+    plaintext = AESPad(plaintext)
+    iv = os.urandom(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return iv + cipher.encrypt(plaintext)
+
+def WhatsAppEncrypt(encKey, macKey, plaintext):
+    enc = AESEncrypt(encKey, plaintext)
+    return HmacSha256(macKey, enc) + enc				# this may need padding to 64 byte boundary
+
+def AESDecrypt(key, ciphertext):						# from https://stackoverflow.com/a/20868265
+    iv = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    return AESUnpad(plaintext)
