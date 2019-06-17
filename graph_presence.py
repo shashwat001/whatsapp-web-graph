5cf3c54ce23afdeac702377124491332914113d5
Shashwat Kumar <shashwatkmr.001@gmail.com>
2019-06-18 01:06:14 +0530

    attempt to generate graph WIP

--- graph_presence.py
+++ graph_presence.py
@@ -3,6 +3,13 @@ from utilities import customTime
 import logging
 from datetime import datetime
 
+import matplotlib
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+from matplotlib.dates import HourLocator
+
 home = expanduser("~")
 settingsDir = home + "/.wweb"
 loggingDir = "./logs"
@@ -33,7 +40,7 @@ def loadPresenceData():
             continue
         if number not in numberData:
             numberData[number] = {}
-            numberData[number]['timesum'] = datetime.strptime("0:00:00", "%H:%M:%S")
+            numberData[number]['timesum'] = datetime.strptime("0:00:01", "%H:%M:%S")
         if 'timeinfo' not in numberData[number]:
             if pType == 'unavailable':
                 continue
@@ -47,11 +54,50 @@ def loadPresenceData():
         else:
             continue
 
+def sortData():
+    ar = []
+    for k,v in numberData.iteritems():
+        ar.append((k, v['timesum']))
+    ar = sorted(ar, key=lambda x: x[1])
+    y_pos = []
+    x_pos = []
+    for l in ar:
+        x_pos.append(l[0])
+        y_pos.append(l[1])
+    return x_pos, y_pos
+
+def generateGraph():
+    objects, performance = sortData()
+    # objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+    y_pos = np.arange(len(objects))
+    # performance = [10,8,6,4,2,1]
+
+    ax = plt.subplot()
+    plt.barh(y_pos, performance, align='center', alpha=0.5)
+    plt.yticks(y_pos, objects)
+    plt.xlabel('Time')
+    plt.title('User')
+    ax.xaxis.set_major_locator(HourLocator())
+    ax.xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
+    plt.show()
+
+def genSO():
+    numbers, times = sortData()
+    y = times
+    ax = plt.subplot()
+    ax.barh(numbers, y, align='center', alpha=0.5)
+    ax.xaxis.set_major_locator(HourLocator())
+    ax.xaxis.set_major_formatter(DateFormatter('%H:%M'))
+    plt.show()
+
 if __name__ == "__main__":
     logging.basicConfig(filename=loggingDir+"/graph.log",format='%(asctime)s - %(message).300s', level=logging.INFO, filemode='w')
     logging.Formatter.converter = customTime
 
     loadPresenceData()
-    for k,v in numberData.iteritems():
-        print("%s: %s" % (k, v['timesum']))
+    # sortData()
+    
+    
     # print(numberData)
+    # generateGraph()
+    genSO()
