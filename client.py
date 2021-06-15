30151bd0114d440018239108e455d9c0f7ed4962
Kathir T <kathir.t@zohocorp.com>
2021-06-15 23:09:16 +0530

    Fixed: #4 Arguments mismatch - on_close()

--- client.py
+++ client.py
@@ -289,7 +289,7 @@ class WhatsApp:
     self.ws = websocket.WebSocketApp("wss://web.whatsapp.com/ws",
                                      on_message=lambda ws, msg: self.on_message(ws, msg),
                                      on_error=lambda ws, msg: self.on_error(ws, msg),
-                                     on_close=lambda ws: self.on_close(ws),
+                                     on_close=lambda ws: self.on_close(),
                                      on_open=lambda ws: self.on_open(ws),
                                      header={"Origin: https://web.whatsapp.com"})
     self.refreshConnectionTimer = Timer(11*60*60, lambda: self.ws.close())
